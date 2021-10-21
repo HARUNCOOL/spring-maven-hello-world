b4e724f2165d058138ec0b2bf2f4619164fe8ab5
markondareddy <bmarkondareddy@yahoo.com>
2021-10-21 07:54:07 +0530

    Updated

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
